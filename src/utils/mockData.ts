dac9b1bc3a204cf38478a2c320284392320cd4fb
anetalizancova <aneta.lizancova@gmail.com>
2025-05-06 16:17:01 +0200

    Update mockData.ts

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -182,7 +182,7 @@ export const getRandomQuote = (progressPercentage: number): Quote => {
 export const formatCurrency = (amount: number): string => {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'CZK',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0
   }).format(amount);
